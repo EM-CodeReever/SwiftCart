bde3da6b301d6b401a56cd7e5165e2b79130b51b
Elliot Morrison <elliotmorrison58@gmail.com>
2023-11-30 14:42:43 -0500

    Add product page

---
+++ src/routes/(public)/product/[productId]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Navbar from '$components/Navbar.svelte';
+    import type { PageData } from './$types';
+    import { page } from '$app/stores';
+    let productId = $page.params.productId;
+    
+    export let data: PageData;
+</script>
+
+<svelte:head>
+    <title>{productId}</title>
+</svelte:head>
+
+<section class="min-h-screen w-full">
+    <Navbar />
+    {productId}
+</section>
