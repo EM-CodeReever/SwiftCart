8ae0b69e08f57bbfaa372f773cc0fd63db7f908d
Elliot Morrison <elliotmorrison58@gmail.com>
2023-11-30 14:40:47 -0500

    Add new components DotWaveLoader, CheckoutIcon, CouponIcon, and SwiftCartLogo

---
+++ src/components/DotWaveLoader.svelte
@@ -0,0 +1,6 @@
+<div class="spinner-wave spinner-sm [--spinner-color:var(--pink-2)]">
+    <div class="spinner-wave-dot"></div>
+    <div class="spinner-wave-dot"></div>
+    <div class="spinner-wave-dot"></div>
+    <div class="spinner-wave-dot"></div>
+</div>
