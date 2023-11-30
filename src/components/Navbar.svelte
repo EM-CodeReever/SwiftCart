e83fda4ca1d912838e2b92bbb94f65b8994cbd11
Elliot Morrison <elliotmorrison58@gmail.com>
2023-11-30 14:41:20 -0500

    Update Navbar styling and add SwiftCart logo

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
     import ShoppingCart from "./svg/ShoppingCart.svelte";
+    import SwiftCartLogo from "./svg/SwiftCartLogo.svelte";
     import UserIcon from "./svg/UserIcon.svelte";
 
     let userSignedIn = false;
@@ -7,9 +9,12 @@
 
 </script>
 
-<div class="navbar p-2 shadow-none bg-gray-300">
+<div class="navbar p-2 shadow-none bg-gray-700">
 	<div class="navbar-start">
-		<a class=" hover:bg-gray-100 p-2 rounded-lg font-bold text-xl">SwiftCart</a>
+		<a href="/" class="hover:bg-[#00000032] p-2 flex space-x-2 rounded-lg text-gray-200 font-bold text-2xl items-center">
+      <SwiftCartLogo />
+      <span>SwiftCart</span>
+    </a>
 	</div>
 	<div class="navbar-center w-full">
 		<span class="flex flex-grow">
@@ -22,13 +27,12 @@
     </span>
 	</div>
 	<div class="navbar-end">
-		
 		<div class="dropdown dropdown-hover">
       <!-- svelte-ignore a11y-label-has-associated-control -->
       <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-      <label tabindex="0" class="flex items-center hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+      <label tabindex="0" class="flex space-x-1 items-center hover:bg-[#00000032] p-2 rounded-lg cursor-pointer">
         <UserIcon />
-        <span class="hidden flex-col items-start sm:flex">
+        <span class="hidden flex-col items-start sm:flex text-gray-200">
           <p class="text-sm">Hello, sign in</p>
           <p class="text-sm -mt-1 font-semibold">Account</p>
         </span>
@@ -42,13 +46,13 @@
         {:else}
         <div class="grid grid-cols-2 w-96">
           <span class="col-span-1 flex-col">
-            <a class="dropdown-item text-sm font-bold">Account Settings</a>
-            <a tabindex="-1" class="dropdown-item text-sm font-bold">Logout</a>
+            <a href="/" class="dropdown-item text-sm font-bold">Account Settings</a>
+            <a href="/" tabindex="-1" class="dropdown-item text-sm font-bold">Logout</a>
           </span>
           <span class="col-span-1 flex flex-col">
-            <a class="dropdown-item text-sm">Purchase History</a>
-            <a tabindex="-1" class="dropdown-item text-sm">Coupons</a>
-            <a tabindex="-1" class="dropdown-item text-sm">Wishlist</a>
+            <a href="/" class="dropdown-item text-sm">Purchase History</a>
+            <a href="/" tabindex="-1" class="dropdown-item text-sm">Coupons</a>
+            <a href="/" tabindex="-1" class="dropdown-item text-sm">Wishlist</a>
           </span>
         </div>
         {/if}
@@ -57,9 +61,9 @@
 		<div class="dropdown dropdown-hover">
       <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
       <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label tabindex="0" class="flex items-center hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+      <label tabindex="0" class="flex items-center hover:bg-[#00000032] p-2 rounded-lg cursor-pointer">
         <ShoppingCart />
-        <span class="flex flex-col items-center justify-around">
+        <span class="flex flex-col items-center justify-around text-gray-200">
           <p class="text-sm font-bold">3</p>
           <p class="text-sm -mt-2">cart</p>
         </span>
@@ -68,30 +72,34 @@
         <span class="flex flex-col space-y-2">
           <p class="px-2 text-sm font-semibold -mb-2">3 items</p>
           <p class="px-2 text-sm">Cart total: $1430</p>
-          <button class="btn btn-primary mt-1">View Cart</button>
+          <button class="btn btn-error mt-1" on:click={()=>{
+            goto("/cart")
+          }}>View Cart</button>
         </span>
       </div>
     </div>
 	</div>
 </div>
-<hr class="border-gray-100">
-<div class="navbar shadow-none p-1 bg-gray-300">
+<!-- <hr class="border-gray-200"> -->
+<div class="navbar shadow-none p-1 bg-gray-800 text-gray-200">
   <div class="navbar-center space-x-2">
-    <a href="" class="nav-item text-sm hover:bg-gray-100 py-1 px-2 rounded-lg font-semibold">Deals</a>
-    <a href="" class="nav-item text-sm hover:bg-gray-100 py-1 px-2 rounded-lg font-semibold">Popular</a>
+    <a href="/" class="nav-item text-sm hover:bg-[#00000032] py-1 px-2 rounded-lg font-semibold">Deals</a>
+    <a href="/" class="nav-item text-sm hover:bg-[#00000032] py-1 px-2 rounded-lg font-semibold">Popular</a>
     <div class="dropdown dropdown-hover">
-      <label tabindex="0" class="nav-item text-sm flex items-center space-x-1 hover:bg-gray-100 py-1 px-2 rounded-lg">
+      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+      <!-- svelte-ignore a11y-label-has-associated-control -->
+      <label tabindex="0" class="nav-item text-sm flex items-center space-x-1 hover:bg-[#00000032] py-1 px-2 rounded-lg">
         <p class="font-semibold">Categories</p> 
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
       </svg>
     </label>
-      <div class="mt-2 dropdown-menu dropdown-menu-bottom-right ">
-        <a class="dropdown-item text-sm">Category 1</a>
-        <a tabindex="-1" class="dropdown-item text-sm">Category 2</a>
-        <a tabindex="-1" class="dropdown-item text-sm">Category 3</a>
-        <a tabindex="-1" class="dropdown-item text-sm">Category 4</a>
-        <a tabindex="-1" class="dropdown-item text-sm">Category 5</a>
+      <div class="mt-2 dropdown-menu dropdown-menu-bottom-right text-gray-800">
+        <a href="/" class="dropdown-item text-sm">Category 1</a>
+        <a href="/" tabindex="-1" class="dropdown-item text-sm">Category 2</a>
+        <a href="/" tabindex="-1" class="dropdown-item text-sm">Category 3</a>
+        <a href="/" tabindex="-1" class="dropdown-item text-sm">Category 4</a>
+        <a href="/" tabindex="-1" class="dropdown-item text-sm">Category 5</a>
       </div>
     </div>
   </div>
