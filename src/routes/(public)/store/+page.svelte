1fe5e454099ac10e84a1ce5e170ac4b6351cdff2
Elliot Morrison <elliotmorrison58@gmail.com>
2023-11-30 12:25:02 -0500

    Delete unnecessary code for categories and store pages

--- src/routes/(public)/store/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
-</script>
-<section class="w-full min-h-screen h-fit bg-gradient-to-r from-sky-400 to-blue-500">
-</section>
