3c3b1dd51c1340f928f04507f988cecdfccb0ab5
Elliot Morrison <elliotmorrison58@gmail.com>
2023-11-30 20:49:32 -0500

    Update +page.svelte with TypeScript and Navbar data prop

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,13 +1,16 @@
-<script>
+<script lang="ts">
     import Navbar from "$components/Navbar.svelte";
-
+    import type { PageData } from "./$types";
+    export let data: PageData;
+    let { supabase, session } = data;
+    $: ({ supabase, session } = data);
 </script>
 
 <svelte:head>
     <title>SwiftCart</title>
 </svelte:head>
 <section class="min-h-screen w-full">
-    <Navbar />
+    <Navbar data={data}/>
     <section class="h-96 w-full bg-gray-300 p-5">
 
     </section>
