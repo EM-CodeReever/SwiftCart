cacebd6c6ed1a2720695286d0a7043c8a6e95d54
Elliot Morrison <elliotmorrison58@gmail.com>
2023-12-08 20:09:13 -0500

    Refactor Navbar component and add AutocompleteInput

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,56 +1,81 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
+    import type { LayoutServerData } from "../routes/$types";
     import type { PageData } from "../routes/(public)/$types";
+    import AutocompleteInput from "./AutocompleteInput.svelte";
     import ShoppingCart from "./svg/ShoppingCart.svelte";
     import SwiftCartLogo from "./svg/SwiftCartLogo.svelte";
     import UserIcon from "./svg/UserIcon.svelte";
     
+    
     export let data: PageData;
-    let { supabase, session } = data;
-    $: ({ supabase, session } = data);
+    let { supabase, session, customer,tempFirstName } = data;
+    $: ({ supabase, session, customer, tempFirstName } = data);
+
+    
 
     let userSignedIn = session != null
     $: userSignedIn = session != null
 
-
-    $: console.log("User signed in: ", userSignedIn)
-    $: if (userSignedIn) {
-      console.log("User email: ", session?.user.email)
-    }
+    let customerSignedIn = customer != null
+    $: customerSignedIn = customer != null
+    
+    
+  if(customer) {
+    console.log("Customer: ", customer)
+    userSignedIn = true
+  }else if(session?.user){
+    console.log("No Profile: ", session?.user)
+    userSignedIn = true
+  }else{
+    console.log("No User")
+    userSignedIn = false
+  }
 
     function signOut() {
       supabase.auth.signOut().then(() => {
         goto("/")
       })
+      
     }
 </script>
 
 <div class="navbar p-2 shadow-none bg-gray-700">
-	<div class="navbar-start">
+	<div class="navbar-start w-fit">
 		<a href="/" class="hover:bg-[#00000032] p-2 flex space-x-2 rounded-lg text-gray-200 font-bold text-2xl items-center">
       <SwiftCartLogo />
       <span>SwiftCart</span>
     </a>
 	</div>
-	<div class="navbar-center w-full">
-		<span class="flex flex-grow">
-      <input class="input input-sm  input-block input-solid placeholder:text-sm rounded-r-none" placeholder="Search..." />
-      <button class="btn btn-sm btn-error rounded-l-none">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>      
-      </button>
+	<div class="navbar-center px-5 w-full">
+		<span class="flex-grow max-w-2xl hidden md:flex">
+      <!-- <input class="" placeholder="Search..." /> -->
+      <AutocompleteInput classNames="input input-sm input-block bg-[#00000032] border-transparent text-gray-200 placeholder:text-sm rounded-r-none">
+        <button class="btn btn-sm bg-[#00000032] rounded-l-none hover:btn-error">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>      
+        </button>
+        </AutocompleteInput>
+      
     </span>
+
 	</div>
-	<div class="navbar-end">
+	<div class="navbar-end w-fit">
 		<div class="dropdown dropdown-hover">
       <!-- svelte-ignore a11y-label-has-associated-control -->
       <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-      <label tabindex="0" class="flex space-x-1 items-center hover:bg-[#00000032] p-2 rounded-lg cursor-pointer">
+      <label tabindex="0" class="flex space-x-1 items-center hover:bg-[#00000032] -mr-1 sm:mr-0 p-1 sm:p-2 rounded-lg cursor-pointer">
         <UserIcon />
         <span class="hidden flex-col items-start sm:flex text-gray-200">
-          <p class="text-sm"> {userSignedIn ? 'Hello, ' + session?.user.email : 'Sign In'}</p>
           <p class="text-sm -mt-1 font-semibold">Account</p>
+          {#if userSignedIn && customerSignedIn}
+          <p class="text-sm">Hello, {customer?.first_name}</p>
+          {:else if userSignedIn}
+          <p class="text-sm">Hello, {tempFirstName}</p>
+          {:else}
+          <p class="text-sm">Sign in</p>
+          {/if}
         </span>
       </label>
       <div class="dropdown-menu my-2 w-fit dropdown-menu-bottom-left">
@@ -79,9 +104,9 @@
 		<div class="dropdown dropdown-hover">
       <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
       <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label tabindex="0" class="flex items-center hover:bg-[#00000032] p-2 rounded-lg cursor-pointer">
+      <label tabindex="0" class="flex items-center hover:bg-[#00000032] p-1 sm:p-2 rounded-lg cursor-pointer ">
         <ShoppingCart />
-        <span class="flex flex-col items-center justify-around text-gray-200">
+        <span class="hidden flex-col items-center justify-around text-gray-200 sm:flex">
           <p class="text-sm font-bold">3</p>
           <p class="text-sm -mt-2">cart</p>
         </span>
@@ -98,29 +123,16 @@
     </div>
 	</div>
 </div>
-<!-- <hr class="border-gray-200"> -->
-<div class="navbar shadow-none p-1 bg-gray-800 text-gray-200">
-  <div class="navbar-center space-x-2">
-    <a href="/" class="nav-item text-sm hover:bg-[#00000032] py-1 px-2 rounded-lg font-semibold">Deals</a>
-    <a href="/" class="nav-item text-sm hover:bg-[#00000032] py-1 px-2 rounded-lg font-semibold">Popular</a>
-    <div class="dropdown dropdown-hover">
-      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-      <!-- svelte-ignore a11y-label-has-associated-control -->
-      <label tabindex="0" class="nav-item text-sm flex items-center space-x-1 hover:bg-[#00000032] py-1 px-2 rounded-lg">
-        <p class="font-semibold">Categories</p> 
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-3 h-3">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-      </svg>
-    </label>
-      <div class="mt-2 dropdown-menu dropdown-menu-bottom-right text-gray-800">
-        <a href="/" class="dropdown-item text-sm">Category 1</a>
-        <a href="/" tabindex="-1" class="dropdown-item text-sm">Category 2</a>
-        <a href="/" tabindex="-1" class="dropdown-item text-sm">Category 3</a>
-        <a href="/" tabindex="-1" class="dropdown-item text-sm">Category 4</a>
-        <a href="/" tabindex="-1" class="dropdown-item text-sm">Category 5</a>
-      </div>
-    </div>
-  </div>
+
+<div class="pb-2 justify-center items-center px-3 sm:px-8 bg-gray-700 flex md:hidden">
+  <span class="flex-grow flex">
+    <input class="input input-sm input-block bg-[#00000032] border-transparent text-gray-200 placeholder:text-sm rounded-r-none" placeholder="Search..." />
+    <button class="btn btn-sm bg-[#00000032] rounded-l-none hover:btn-error">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+      </svg>      
+    </button>
+  </span>
 </div>
 
 
