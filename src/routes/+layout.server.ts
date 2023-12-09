392f8735d67b8a4573a63ea5c557682bc764aca9
Elliot Morrison <elliotmorrison58@gmail.com>
2023-12-08 20:09:40 -0500

    Update layout server load function

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,22 @@
-export const load = async ({ locals: { getSession } }) => {
-    return {
-      session: await getSession(),
+import { PrismaClient } from '@prisma/client';
+let prisma = new PrismaClient();
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async ({ locals: { getSession },cookies }) => {
+    const session = await getSession()
+
+    let customer
+    if (session?.user) {
+      customer = await prisma.customer.findFirst({
+            where: {
+                email: session.user.email
+            }
+        });
     }
+    return {
+      customer,
+      tempLastName: cookies.get('lname'),
+      tempFirstName: cookies.get('fname'),
+      session
+    };
   }
