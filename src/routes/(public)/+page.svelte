a807edf5b4ca8228d640853df617b0f9d77d68d5
Elliot Morrison <elliotmorrison58@gmail.com>
2023-12-08 20:10:00 -0500

    Add Splide slider and AutocompleteInput component

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
     import Navbar from "$components/Navbar.svelte";
+    import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
+    import '@splidejs/svelte-splide/css';
     import type { PageData } from "./$types";
+    import { browser } from "$app/environment";
+    import { onMount } from "svelte";
+    import HomeSlide from "$components/HomeSlide.svelte";
+    import AutocompleteInput from "$components/AutocompleteInput.svelte";
     export let data: PageData;
     let { supabase, session } = data;
     $: ({ supabase, session } = data);
+    
+
 </script>
 
 <svelte:head>
@@ -11,7 +19,43 @@
 </svelte:head>
 <section class="min-h-screen w-full">
     <Navbar data={data}/>
-    <section class="h-96 w-full bg-gray-300 p-5">
+    <Splide options={ {
+        rewind  : true,
+        pauseOnHover: false,
+        gap     : '1rem',
+        autoplay: true,
+        arrows  : true,
+        height  : 'fit-content',
+        // width   : '100%',
+      } } hasTrack={ false } class="bg-[#f31260] p-1 w-full">
+        <div style="position: relative">
+          <SplideTrack>
+              <SplideSlide class="flex justify-center items-center md:space-x-10 px-10 lg:px-20 xl:px-36 py-10" >
+                  <HomeSlide name="Lorem Ipsum"/>
+              </SplideSlide>
+              <SplideSlide class="flex justify-center items-center md:space-x-10 px-10 lg:px-20 xl:px-36 py-10" >
+                <HomeSlide name="Lorem Ipsum 2"/>
+            </SplideSlide>
+            <SplideSlide class="flex justify-center items-center md:space-x-10 px-10 lg:px-20 xl:px-36 py-10" >
+              <HomeSlide name="Lorem Ipsum 3"/>
+          </SplideSlide>
 
-    </section>
+              
+          </SplideTrack>
+        </div>
+        <div class="splide__progress mt-3">
+          <div class="splide__progress__bar">
+          </div>
+        </div>
+        <button class="splide__toggle hidden" type="button">
+          <span class="splide__toggle__play">Play</span>
+          <span class="splide__toggle__pause">Pause</span>
+        </button>
+      </Splide>
 </section>
+
+<style>
+    .splide__progress__bar{
+      background-color: #000;
+    }
+  </style>
