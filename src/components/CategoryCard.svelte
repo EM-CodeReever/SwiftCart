bc6d8c3904b56d318c97bcad5377c1466af6ffbd
Elliot Morrison <elliotmorrison58@gmail.com>
2023-12-14 21:37:37 -0500

    Add CategoryCard component

---
+++ src/components/CategoryCard.svelte
@@ -0,0 +1,14 @@
+<div class="relative">
+    <div
+        class="relative w-full h-80 bg-trasnparent rounded-lg overflow-hidden hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer"
+    >
+        <img
+            alt="soething"
+            src="https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v1._SY304_CB573698005_.jpg"
+            class="w-full h-full object-center object-cover"
+        />
+    </div>
+    <h3 class="mt-3 font-medium text-xl text-gray-800">
+        <a href="/"> Lorem Ipsum </a>
+    </h3>
+</div>
