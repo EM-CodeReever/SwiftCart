2a06193aa070852e5e9a1e8271f6c55e4ceaac47
Elliot Morrison <elliotmorrison58@gmail.com>
2023-12-16 20:31:42 -0500

    Refactor AutocompleteInput component to use dynamic product names

--- src/components/AutocompleteInput.svelte
+++ src/components/AutocompleteInput.svelte
@@ -1,92 +1,13 @@
 <script lang="ts">
     import { browser } from "$app/environment";
     import { goto, invalidateAll } from "$app/navigation";
+    import type { DJ_Product } from "$lib";
     import { fade } from "svelte/transition";
     export let classNames = "";
+    export let products: DJ_Product[];
+    let productNames = products.map((product) => product.title);
     let value = "";
-    let fruits = [
-      'Apple',
-      'Apricot',
-      'Avocado',
-      'Banana',
-      'Bilberry',
-      'Blackberry',
-      'Blackcurrant',
-      'Blueberry',
-      'Boysenberry',
-      'Blood Orange',
-      'Cantaloupe',
-      'Currant',
-      'Cherry',
-      'Cherimoya',
-      'Cloudberry',
-      'Coconut',
-      'Cranberry',
-      'Clementine',
-      'Damson',
-      'Date',
-      'Dragonfruit',
-      'Durian',
-      'Elderberry',
-      'Feijoa',
-      'Fig',
-      'Goji berry',
-      'Gooseberry',
-      'Grape',
-      'Grapefruit',
-      'Guava',
-      'Honeydew',
-      'Huckleberry',
-      'Jabouticaba',
-      'Jackfruit',
-      'Jambul',
-      'Jujube',
-      'Juniper berry',
-      'Kiwi fruit',
-      'Kumquat',
-      'Lemon',
-      'Lime',
-      'Loquat',
-      'Lychee',
-      'Nectarine',
-      'Mango',
-      'Marion berry',
-      'Melon',
-      'Miracle fruit',
-      'Mulberry',
-      'Mandarine',
-      'Olive',
-      'Orange',
-      'Papaya',
-      'Passionfruit',
-      'Peach',
-      'Pear',
-      'Persimmon',
-      'Physalis',
-      'Plum',
-      'Pineapple',
-      'Pumpkin',
-      'Pomegranate',
-      'Pomelo',
-      'Purple Mangosteen',
-      'Quince',
-      'Raspberry',
-      'Raisin',
-      'Rambutan',
-      'Redcurrant',
-      'Salal berry',
-      'Satsuma',
-      'Star fruit',
-      'Strawberry',
-      'Squash',
-      'Salmonberry',
-      'Tamarillo',
-      'Tamarind',
-      'Tomato',
-      'Tangerine',
-      'Ugli fruit',
-      'Watermelon',
-    ]
+
     /* HANDLING THE INPUT */
 let searchInput: HTMLInputElement; // use with bind:this to focus element
 let inputValue = "";
@@ -97,7 +18,7 @@ let hiLiteIndex:number | null;
 const filterItems = () => {
 	let storageArr = <any>[]
 	if (inputValue) {
-		fruits.forEach(item => {
+		productNames.forEach(item => {
 			 if (item.toLowerCase().startsWith(inputValue.toLowerCase())) {
 				 storageArr = [...storageArr, item];
 			 }
