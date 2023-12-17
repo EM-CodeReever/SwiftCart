27a8332382a7da1337f12b46b6398cae5268f3f0
Elliot Morrison <elliotmorrison58@gmail.com>
2023-12-16 20:31:32 -0500

    Add DJ_Product type definition

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,17 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
+
+export type DJ_Product = {
+        id: number,
+        title: string,
+        description: string,
+        price: number,
+        discountPercentage: number,
+        rating: number,
+        stock: number,
+        brand: string,
+        category: string,
+        thumbnail: string,
+        images: string[],
+      }
